0a53b9177fab5a9864ed8bd0898efbf0b3cb90bc
Susan S. <200622750@student.georgianc.on.ca>
2025-10-06 11:30:46 -0400

    added  header tag and form with 3 inputs + register button

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -10,5 +10,34 @@ type FormData = {
 
 export default function RegisterPage() {
   const { register, handleSubmit, formState: { errors }, watch, } = useForm<FormData>();
+  return (
+    <div>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Username */}
+            <fieldset>
+            <label>Username</label>
+            <input {...register('username', { required: 'Username is required' })}/>
+            {errors.username && <p>{errors.username.message}</p>}
+            </fieldset>
+        {/* Password */}
+            <fieldset>
+            <label>Password</label>
+            <input type="password" {...register('password', {required: 'Password is required', minLength: {value: 6, message: 'Password must be at least 6 characters',},})}/>
+            {errors.password && <p>{errors.password.message}</p>}
+            </fieldset>
+        {/* Confirm Password */}
+            <fieldset>
+                <label>Confirm Password</label>
+                <input type="password" {...register('confirm', {required: 'Please confirm your password',validate: (value) => value === password || 'Passwords do not match',})}/>
+                {errors.confirm && <p>{errors.confirm.message}</p>}
+            </fieldset>
+            
+            <button type="submit">Register</button>
 
+        </form>
+    </div>
+         
+
+  );
 }
